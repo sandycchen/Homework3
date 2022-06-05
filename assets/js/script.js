287e5b31ec5c31bca54bb46081cb6d81a6c39352
Sandy <sandycchen30@gmail.com>
2022-06-04 22:45:31 -0700

    added prompt validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,116 @@
 // Assignment code here
 
+var lowerCasedCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCasedCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialCharacters = ["'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
 
-var generatePassword = function(){
-  window.alert("Welcome to Password Generator!");
-  selectCriteria();
-};
+function getPasswordOptions() {
+  var length = parseInt(
+    prompt('How many characters would you like your password to have?')
+  );
 
-var selectCriteria = function() {
-  var selectOption = window.prompt("Would you like to include CHARACTER LENGTH and CHARACTER TYPES in the password? Please enter 1 for CHARACTER LENGTH, 2 for CHARACTER TYPES, or 3 for BOTH.");
-
-  selectOption = parseInt(selectOption);
-  switch (selectOption) {
-    case 1:
-      
-
-
-
-
-
-
+  if (Number.isNaN(length)) {
+    alert('Password length must be provided as a number.');
+    return null;
   }
 
+  if (length < 8) {
+    alert('Password length must be at least 8 characters.');
+    return null;
+  }
 
+  if (length > 128) {
+    alert('Password length must less than 129 characters.');
+    return null;
+  }
 
+  var hasSpecialCharacters = confirm(
+    'Click OK to confirm including special characters.'
+  );
 
+  var hasNumericCharacters = confirm(
+    'Click OK to confirm including numeric characters.'
+  );
 
+  var hasLowerCasedCharacters = confirm(
+    'Click OK to confirm including lowercase characters.'
+  );
+
+  var hasUpperCasedCharacters = confirm(
+    'Click OK to confirm including uppercase characters.'
+  );
+
+  if (
+    hasSpecialCharacters === false &&
+    hasNumericCharacters === false &&
+    hasLowerCasedCharacters === false &&
+    hasUpperCasedCharacters === false
+  ) {
+    alert('You must select at least one character type!');
+    return null;
+  }
+
+  var passwordOptions = {
+    length: length,
+    hasSpecialCharacters: hasSpecialCharacters,
+    hasNumericCharacters: hasNumericCharacters,
+    hasLowerCasedCharacters: hasLowerCasedCharacters,
+    hasUpperCasedCharacters: hasUpperCasedCharacters
+  };
+
+  return passwordOptions;
 }
 
-var pwdLength = function() {
-  window.prompt
+function getRandom(arr) {
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
+
+  return randElement;
 }
 
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-  //window.prompt
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-  //case (shop)
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-  //Math.random
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-  //confirm function
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-  //pick valid option
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-  //for loop
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-  //window.alert
+function generatePassword() {
+  var options = getPasswordOptions();
+  var result = [];
 
+  var possibleCharacters = [];
 
+  var guaranteedCharacters = [];
 
+  if (!options) return null;
+
+  if (options.hasSpecialCharacters) {
+    possibleCharacters = possibleCharacters.concat(specialCharacters);
+    guaranteedCharacters.push(getRandom(specialCharacters));
+  }
+
+  if (options.hasNumericCharacters) {
+    possibleCharacters = possibleCharacters.concat(numericCharacters);
+    guaranteedCharacters.push(getRandom(numericCharacters));
+  }
+
+  if (options.hasLowerCasedCharacters) {
+    possibleCharacters = possibleCharacters.concat(lowerCasedCharacters);
+    guaranteedCharacters.push(getRandom(lowerCasedCharacters));
+  }
+
+  if (options.hasUpperCasedCharacters) {
+    possibleCharacters = possibleCharacters.concat(upperCasedCharacters);
+    guaranteedCharacters.push(getRandom(upperCasedCharacters));
+  }
+
+  for (var i = 0; i < options.length; i++) {
+    var possibleCharacter = getRandom(possibleCharacters);
+
+    result.push(possibleCharacter);
+  }
+
+  for (var i = 0; i < guaranteedCharacters.length; i++) {
+    result[i] = guaranteedCharacters[i];
+  }
+
+  return result.join('');
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
